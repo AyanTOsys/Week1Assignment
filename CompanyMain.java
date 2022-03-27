fc12281917838709cc7804b7e70aec31d20cffec
AyanTOsys <102143190+AyanTOsys@users.noreply.github.com>
2022-03-27 10:52:37 +0530

    methods called

--- CompanyMain.java
+++ CompanyMain.java
@@ -1,11 +1,22 @@
 package company;
 
+import trading.Shirt;
+import trading.Toys;
+import trading.TwoWheeler;
+
 public class CompanyMain {
 
 	public static void main(String[] args) {
 		KFC kfcObj= new KFC();
 		kfcObj.produceFood();
 		kfcObj.produceGasBill();
+		kfcObj.showReportcard();
+		
+		Toys tObj = new Toys();
+		Shirt sObj = new Shirt();
+		TwoWheeler twObj =new TwoWheeler();
+		
+		kfcObj.doTrading(tObj, sObj, twObj);
 	}
 
 }
