069ec4c672b96d7a56214a03cbca40ee996fde2a
AyanTOsys <102143190+AyanTOsys@users.noreply.github.com>
2022-03-27 10:49:32 +0530

    classroom package

---
+++ Book.java
@@ -0,0 +1,7 @@
+package classroom;
+
+public class Book {
+	HolidayHome hhObj =new HolidayHome();
+	Movie mObj =new Movie();
+	LearningGuitar lgObj = new LearningGuitar();
+}
---
+++ Classroom.java
@@ -0,0 +1,8 @@
+package classroom;
+
+public class Classroom {
+	Library lbObj = new Library();
+	Aquarium aqObj = new Aquarium();
+	Book bObj = new Book();
+	Laptop lpObj =new Laptop();
+}
